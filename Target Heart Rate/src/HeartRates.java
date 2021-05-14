27bcae51b2e859b80fe8e793d9a698ee5e6aa0f5
Omole Priscilla <75090243+Priscilla-Omole@users.noreply.github.com>
2021-05-14 13:41:27 +0100

    Add files via upload

---
+++ Target Heart Rate/src/HeartRates.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class HeartRates {
+
+    public static void attributes(String FirstName, String lastName, int DayOfBirth, int monthOfBirth, int yearOfBirth)
+    {
+        public HeartRates ( String firstName)
+    {
+      FirstName= firstName;
+    }
+
+    }
+}
